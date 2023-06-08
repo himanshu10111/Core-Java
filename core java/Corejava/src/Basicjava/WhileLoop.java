74f11a49d58af3496c73ff8668bad1707425cfc9
Himanshu Doye <himanshu.d@riequation.com>
2023-06-08 11:38:13 +0530

    while loop added

---
+++ core java/Corejava/src/Basicjava/WhileLoop.java
@@ -0,0 +1,23 @@
+package Basicjava;
+
+import java.util.Scanner;
+
+public class WhileLoop {
+public static void main(String[] args) {
+
+    Scanner sc =  new Scanner(System.in);
+
+    System.out.println("Enter the number for multiplication");
+    int num = sc.nextInt();
+
+    int i=1;
+
+    while(i<=10)
+    {
+        System.out.println(num+" * "+"i"+" = "+num*i);
+        i++;
+    }
+    
+
+}
+}
