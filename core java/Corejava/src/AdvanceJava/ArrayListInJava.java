c5de3fac1a6c991a693f416cbae06c8f55350363
Himanshu Doye <himanshu.d@riequation.com>
2023-06-22 09:57:46 +0530

    adding Array List file

---
+++ core java/Corejava/src/AdvanceJava/ArrayListInJava.java
@@ -0,0 +1,47 @@
+package AdvanceJava;
+
+import java.util.ArrayList;
+
+public class ArrayListInJava {
+public static void main(String[] args) {
+	
+	ArrayList<String> books = new ArrayList<String>(); // create Array List
+	 
+	// Add some Books name in Array List
+	
+	books.add("Python");
+	books.add("java");
+	books.add("Ruby");
+	books.add("Perl");
+	books.add("R");
+	books.add("C#");
+	
+	// Print Them
+	System.out.println(books.get(0));
+	System.out.println(books.get(1));
+	
+	//Change the values "or we can say that update the values"
+	books.set(0,"React");
+	System.out.println(books.get(0));
+	
+	// delete the values 
+	books.remove(0);
+	System.out.println(books.get(0));
+	
+	// delete all values
+	//books.clear();
+
+	
+	// check how many elements in array list.
+
+	System.out.println(books.size());
+	
+	// print all members of Array list..
+	
+	for(String i : books)
+	{
+		System.out.println(i);
+	}
+}
+}
+
