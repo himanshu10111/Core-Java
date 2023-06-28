29abcb748e0f34efd106493a7a32f2159c9af282
Himanshu Doye <himanshu.d@riequation.com>
2023-06-28 06:44:30 +0530

    adding wrapper class file

---
+++ core java/Corejava/src/AdvanceJava/WrapperClassInJava.java
@@ -0,0 +1,18 @@
+package AdvanceJava;
+
+public class WrapperClassInJava {
+	public static void main(String[] args) {
+		
+		Integer i1=Integer.valueOf("123");  // value of method is used to set the data.
+		
+		int i = Integer.parseInt("45"); // parse method is used to type cast the data..
+		
+	
+		System.out.println(i1.intValue()); // value method is used to retrive the data..
+		
+		
+		
+		
+	}
+
+}
